ef9a42991cb73c86c4040a0795faf01c486d41d6
Anthony Gresk <tonygr@hotmail.com>
2020-04-23 17:26:41 -0500

    Layout Design Complete

---
+++ TechTonYc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechTonYc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
